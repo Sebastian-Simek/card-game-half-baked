e5185361935b91cb8422270bbdcd144c19427eda
Sebastian-Simek <sebastiansimek09@gmail.com>
2022-07-11 16:25:04 -0700

    removed test

--- src/ExecutePassButton.js
+++ src/ExecutePassButton.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import Card from './Card';
+import { useCardContext } from './ContextProvider';
 
-export default function ExecutePassButton({ passCard, from, to, selectedCard }) {
+export default function ExecutePassButton({ passCard, selectedCard }) {
+  const { from, to } = useCardContext;
   return (
     <div className="execute-button" onClick={() => passCard(selectedCard)}>
       Pass <Card card={selectedCard} /> from {from} to {to}
