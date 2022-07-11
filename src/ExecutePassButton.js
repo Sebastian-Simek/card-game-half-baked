ab08f4128d45330e9df418a846fb13b7dbed5ac2
Sebastian-Simek <sebastiansimek09@gmail.com>
2022-07-11 15:11:56 -0700

    broke more things

--- src/ExecutePassButton.js
+++ src/ExecutePassButton.js
@@ -3,8 +3,8 @@ import Card from './Card';
 
 export default function ExecutePassButton({ passCard, from, to, selectedCard }) {
   return (
-    <div className='execute-button' onClick={() => passCard(selectedCard)}>
-        Pass <Card card={selectedCard} /> from {from} to {to}
+    <div className="execute-button" onClick={() => passCard(selectedCard)}>
+      Pass <Card card={selectedCard} /> from {from} to {to}
     </div>
   );
 }
